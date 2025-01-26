e773494a512b665c28c38c9c7ddde2de376ca1cd
benson <benson99.lee@gmail.com>
2025-01-26 19:47:07 +0800

    first commit

---
+++ src/main/java/qq/benson/DockerSpringBootTomcatApplication.java
@@ -0,0 +1,13 @@
+package qq.benson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qq/benson/DockerSpringBootTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package qq.benson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
